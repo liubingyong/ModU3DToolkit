6da022ffba3d221235cd45b3a535963f79e77c51
liubingyong <liubingyong@outlook.com>
2016-11-30 20:10:16 +0800

    no message

--- UI/BasePopup.cs
+++ UI/BasePopup.cs
@@ -2,35 +2,38 @@
 using System.Collections;
 
 [RequireComponent(typeof(UIPopup))]
-public abstract class BasePopup : MonoBehaviour {
-	protected UIPopup currentUIPopup;
+public abstract class BasePopup : MonoBehaviour
+{
+    protected UIPopup currentUIPopup;
 
-	public virtual void Awake() {
-		currentUIPopup = this.GetComponent<UIPopup> ();
-	}
+    public virtual void Awake()
+    {
+        currentUIPopup = this.GetComponent<UIPopup>();
+    }
 
-	public virtual void Update() {
-		if (Input.GetKeyUp(KeyCode.Escape) && UIEnterExitTweens.activeTweensCounter == 0)
-		{
-			StartCoroutine(PopPopup());
-		}
-	}
+    public virtual void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && UIEnterExitTweens.activeTweensCounter == 0)
+        {
+            StartCoroutine(PopPopup());
+        }
+    }
 
-	protected void OnShow(UIPopup popup)
-	{
-		popup.BroadcastMessage("StartEnterTweens");
-	}
+    protected void OnShow(UIPopup popup)
+    {
+        popup.BroadcastMessage("StartEnterTweens", SendMessageOptions.DontRequireReceiver);
+    }
 
-	protected IEnumerator PopPopup()
-	{
-		this.BroadcastMessage("StartExitTweens");
+    protected IEnumerator PopPopup()
+    {
+        this.BroadcastMessage("StartExitTweens");
 
-		while (UIEnterExitTweens.activeTweensCounter > 0)
-		{
-			yield return null;
-		}
+        while (UIEnterExitTweens.activeTweensCounter > 0)
+        {
+            yield return null;
+        }
 
-		Manager<UIManager>.Get().PopPopup();
-		Manager<UIManager>.Get().ActivePage.BroadcastMessage("StartEnterTweens", SendMessageOptions.DontRequireReceiver);
-	}
+        Manager<UIManager>.Get().PopPopup();
+        Manager<UIManager>.Get().ActivePage.BroadcastMessage("StartEnterTweens", SendMessageOptions.DontRequireReceiver);
+    }
 }
