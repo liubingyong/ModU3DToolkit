f2d08117825ad23c9153ca582dd7a6d3b0e9cde4
liubingyong <liubingyong@outlook.com>
2016-12-07 19:19:22 +0800

    Add OffScreenIndicator

---
+++ OffScreenIndicator/OffScreenIndicator.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OffScreenIndicator : MonoBehaviour
+{
+    public Sprite indicatorSprite;
+
+    public Vector2 padding;
+
+    public float scale = 1f;
+
+    public bool isActive = true;
+
+    public Image IndicatorImage
+    {
+        get
+        {
+            return indicatorImage;
+        }
+    }
+
+    #region Private
+
+    private Image indicatorImage;
+    private OffScreenIndicatorController controller;
+
+    #endregion
+
+    void Start()
+    {
+        if (!indicatorSprite)
+        {
+            Debug.LogError(" Please, specify the indicator sprite.");
+        }
+        GameObject indicatorImageObject = new GameObject("Indicator");
+        indicatorImageObject.AddComponent<Image>();
+        controller = OffScreenIndicatorController.Instance;
+        if (!controller)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        indicatorImageObject.transform.SetParent(controller.transform);
+        indicatorImage = indicatorImageObject.GetComponent<Image>();
+        indicatorImage.sprite = indicatorSprite;
+        indicatorImage.rectTransform.pivot = new Vector2(0.5f, 1);
+        indicatorImage.rectTransform.localPosition = Vector3.zero;
+        indicatorImage.rectTransform.localScale = Vector3.one * scale;
+        indicatorImage.rectTransform.sizeDelta = new Vector2(indicatorSprite.rect.width, indicatorSprite.rect.height);
+        indicatorImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.CheckIn(this);
+    }
+
+    #region Custom methods
+
+    public void Show()
+    {
+        indicatorImage.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        indicatorImage.gameObject.SetActive(false);
+    }
+
+    public bool IsVisible()
+    {
+        return indicatorImage.gameObject.activeSelf;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return gameObject.transform.position;
+    }
+
+    public void SetLocalPos(Vector3 pos)
+    {
+        indicatorImage.rectTransform.localPosition = pos;
+    }
+
+    public void SetLocalRot(Quaternion rot)
+    {
+        indicatorImage.rectTransform.localRotation = rot;
+    }
+
+    public void SetOpacity(float opacity)
+    {
+        indicatorImage.color = new Color(1.0f, 1.0f, 1.0f, opacity);
+    }
+
+    #endregion
+}
---
+++ OffScreenIndicator/OffScreenIndicatorController.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class OffScreenIndicatorController : MonoBehaviour
+{
+    #region Singleton
+    public static OffScreenIndicatorController Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                OffScreenIndicatorController[] controllers = GameObject.FindObjectsOfType<OffScreenIndicatorController>();
+
+                if (controllers.Length != 0)
+                {
+                    if (controllers.Length == 1)
+                    {
+                        _instance = controllers[0];
+                    }
+                    else
+                    {
+                        Debug.LogError("You have more than one OffScreenIndicatorController in the scene.");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("You should add OffScreenIndicatorController prefab to your canvas");
+                }
+
+
+            }
+
+
+            return _instance;
+        }
+    }
+
+    private static OffScreenIndicatorController _instance;
+    #endregion
+
+    #region Public
+
+    public Vector2 padding = Vector2.zero;
+
+    #endregion
+
+
+    #region Private
+
+    private RectTransform mapRect;
+
+    private float screenSlopAngel;
+
+    #endregion
+
+    #region Unity Methods
+
+    void Awake()
+    {
+        mapRect = GetComponent<RectTransform>();
+        screenSlopAngel = Mathf.Atan2(Screen.height, Screen.width);
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Custom methods
+
+    public void CheckIn(OffScreenIndicator indicator)
+    {
+        if (indicator.isActive)
+        {
+            var screenPos = Camera.main.WorldToScreenPoint(indicator.transform.position);
+
+            if (screenPos.x > 0 && screenPos.x < Screen.width
+                && screenPos.y > 0 && screenPos.y < Screen.height)
+            {
+                // ON SCREEN
+
+                if (indicator.IsVisible())
+                    indicator.Hide();
+            }
+            else
+            {
+                // OFF SCREEN
+                indicator.Show();
+
+                var screenCenter = new Vector3(Screen.width, Screen.height, 0) / 2f;
+
+                screenPos -= screenCenter;
+
+                var slope = screenPos.y / screenPos.x;
+                var angel = Mathf.Atan2(screenPos.y, screenPos.x);
+
+                if (angel < 0)
+                {
+                    angel += 2 * Mathf.PI;
+                }
+
+                if (angel >= 2 * Mathf.PI - screenSlopAngel || angel <= screenSlopAngel)
+                {
+                    screenPos.x = Screen.width / 2;
+                    screenPos.y = slope * screenPos.x;
+                }
+                else if (angel >= screenSlopAngel && angel <= Mathf.PI - screenSlopAngel)
+                {
+                    screenPos.y = Screen.height / 2;
+                    screenPos.x = screenPos.y / slope;
+                }
+                else if (angel >= Mathf.PI - screenSlopAngel && angel <= Mathf.PI + screenSlopAngel)
+                {
+                    screenPos.x = -Screen.width / 2;
+                    screenPos.y = slope * screenPos.x;
+                }
+                else if (angel >= Mathf.PI + screenSlopAngel && angel <= 2 * Mathf.PI - screenSlopAngel)
+                {
+                    screenPos.y = -Screen.height / 2;
+                    screenPos.x = screenPos.y / slope;
+                }
+
+                indicator.SetLocalPos(new Vector3(screenPos.x - padding.x - indicator.padding.x, screenPos.y - padding.y - indicator.padding.y, 0));
+                indicator.SetLocalRot(Quaternion.Euler(0, 0, (angel - Mathf.PI / 2) * Mathf.Rad2Deg));
+            }
+        }
+        else
+        {
+            if (indicator.IsVisible())
+                indicator.Hide();
+        }
+    }
+
+    #endregion
+}
